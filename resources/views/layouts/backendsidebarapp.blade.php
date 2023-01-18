a090ae36f35f9af54d5912219acd2b88a409eaba
mdparvej123 <pj.parvaz45@gmail.com>
2023-01-18 11:32:25 +0600

    last

--- resources/views/layouts/backendsidebarapp.blade.php
+++ resources/views/layouts/backendsidebarapp.blade.php
@@ -13,7 +13,7 @@
         <a href="javascript:;" class=" side-menu {{ request()->routeIS('category.*') ? 'side-menu side-menu--active side-menu--open' : '' }} ">
             <div class="side-menu__icon"> <i data-feather="codepen"></i> </div>
             <div class="side-menu__title">
-                Catagory
+                Catagfry
                 <div class="side-menu__sub-icon"> <i data-feather="chevron-down"></i> </div>
             </div>
         </a>
